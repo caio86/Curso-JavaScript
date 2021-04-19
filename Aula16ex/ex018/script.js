311b384d48a3449a1415bebf836374c6382ff554
Caio86 <caioluiz86@gmail.com>
2021-04-19 19:10:11 -0300

    Aula de exercícios (Parte 7)
    Analisador de Números

--- Aula16ex/ex018/script.js
+++ Aula16ex/ex018/script.js
@@ -1,35 +1,28 @@
-let iNum = document.getElementById('fNum')
-let select = document.getElementById('fSel')
-let res = document.getElementById('res')
-let n
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
 let valores = []
-let soma = 0
-let div = document.createElement('div')
-div.setAttribute('id', 'resultado')
-function adicionar() {
-    n = Number(iNum.value)
-    if (n > 100 || n < 1 || iNum.length == 0 || valores.indexOf(n) != -1) {
-        alert('Valor inválido ou já encontrado na lista')
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
     } else {
-        soma += n
-        valores.push(n)
-        let option = document.createElement('option')
-        option.text = `Valor ${n} adicionado.`
-        select.appendChild(option)
+        return false
     }
-    iNum.value = ''
-    valores.sort((a,b) => a-b)
-    div.innerHTML = ''
 }
-function fim() {
-    if (valores.length == 0) {
-        alert('Adicione valóres antes de finalizar!')
+
+function isLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
     } else {
-        div.innerHTML = `<p>Ao todo, temos ${valores.length} números cadastrados.</p>`
-    div.innerHTML += `<p>O maior valor informado foi ${valores[valores.length-1]}.</p>`
-    div.innerHTML += `<p>O menor valor informado foi ${valores[0]}.</p>`
-    div.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
-    div.innerHTML += `<p>A média dos valores digitados é ${soma/valores.length}.</p>`
-    res.appendChild(div)
+        return false
+    }
+}
+
+function adicionar() {
+    if (isNumero(num.value) && !isLista(num.value, valores)) {
+        
+    } else {
+        alert('Valor inválido ou já encontrado na lista.')
     }
 }
