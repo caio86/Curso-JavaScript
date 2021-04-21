677a8b1b819707f4ec8201152ac3b1f28e2e4e77
Caio86 <caioluiz86@gmail.com>
2021-04-20 23:17:18 -0300

    Aula de exercícios (Parte 8)
    Analisador de Números (Final)

--- Aula16ex/ex018/script.js
+++ Aula16ex/ex018/script.js
@@ -11,7 +11,7 @@ function isNumero(n) {
     }
 }
 
-function isLista(n, l) {
+function inLista(n, l) {
     if (l.indexOf(Number(n)) != -1) {
         return true
     } else {
@@ -20,9 +20,49 @@ function isLista(n, l) {
 }
 
 function adicionar() {
-    if (isNumero(num.value) && !isLista(num.value, valores)) {
-        
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         alert('Valor inválido ou já encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        /* valores.sort((a,b)=>a-b)
+        maior = valores[tot-1]
+        menor = valores[0]
+        for(let pos in valores){
+            soma += valores[pos]
+        } */
+        for(let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos]>maior) {
+                maior = valores[pos]
+            }
+            if (valores[pos]<menor) {
+                menor = valores[pos]
+            }
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}.`
     }
 }
