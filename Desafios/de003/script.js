e77216581f5d7528a263e5e852d57abc261290fe
Caio86 <caioluiz86@gmail.com>
2021-04-23 00:42:14 -0300

    Desafio 3
    Antes e depois

---
+++ Desafios/de003/script.js
@@ -0,0 +1,6 @@
+function botao(){
+    let n = parseInt(prompt('Digite um número inteiro qualquer'))
+    alert(
+        `Antes de ${n}, temos o número ${n-1}\nDepois de ${n}, temos o número ${n+1}`
+    )
+}
