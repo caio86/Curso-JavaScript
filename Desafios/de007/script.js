09e7d6e44120da83f1519230b6819051edfb9269
Caio86 <caioluiz86@gmail.com>
2021-04-30 23:51:51 -0300

    Desafio 7
    Desafio do Conversor de moedas

---
+++ Desafios/de007/script.js
@@ -0,0 +1,16 @@
+let cot = Number.parseFloat(prompt('Antes de mais nada. Quanto está a cotação do dólar agora?', '4.95').replace(',','.'))
+let resultado = document.getElementById('res')
+
+function converte() {
+    let reais = parseFloat(prompt('Quantos R$ você tem na carteira?', '250.50').replace(',','.'))
+    let dolar = reais / cot
+    let $real = {
+        style: 'currency',
+        currency: 'brl'
+    }
+    let $dolar = {
+        style: 'currency',
+        currency: 'usd'
+    }
+    resultado.innerHTML = (`<p>O valor ${reais.toLocaleString('pt-BR', $real)} em dólares é ${dolar.toLocaleString('pt-BR', $dolar)}<p>`)
+}
